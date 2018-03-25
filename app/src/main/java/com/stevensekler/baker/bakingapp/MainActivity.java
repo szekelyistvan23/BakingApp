c8057770172d6eed9ae4fd56cfe645e5fcebf4b5
Szekely Istvan <steven.sekler@gmail.com>
2018-03-25 09:08:07 +0300

    feat: Testing Retrofit and Gson

--- app/src/main/java/com/stevensekler/baker/bakingapp/MainActivity.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/MainActivity.java
@@ -2,6 +2,23 @@ package com.stevensekler.baker.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.stevensekler.baker.bakingapp.model.Cake;
+import com.stevensekler.baker.bakingapp.utils.InternetClient;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://d17h27t6h515a5.cloudfront.net/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        InternetClient client = retrofit.create(InternetClient.class);
+        Call<String> call = client.cakesData();
+
+        call.enqueue(new Callback<String>() {
+
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+//                Toast.makeText(MainActivity.this,response.body().toString(),Toast.LENGTH_SHORT).show();
+
+                Type cakeListType = new TypeToken<ArrayList<Cake>>(){}.getType();
+                List<Cake> cakes = new Gson().fromJson(response.body().toString(), cakeListType);
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(MainActivity.this,"No Internet connection!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/stevensekler/baker/bakingapp/utils/InternetClient.java
@@ -0,0 +1,14 @@
+package com.stevensekler.baker.bakingapp.utils;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Interface for Retrofit, the implementation is based on:
+ * https://www.youtube.com/watch?v=R4XU8yPzSx0
+ */
+
+public interface InternetClient {
+    @GET("/topher/2017/May/59121517_baking/baking.json")
+    Call<String> cakesData();
+}
