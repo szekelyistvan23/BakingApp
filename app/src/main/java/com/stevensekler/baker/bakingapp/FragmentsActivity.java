1ab0f1cbebb05219c541e1776785b2476091dac3
Szekely Istvan <steven.sekler@gmail.com>
2018-03-28 22:34:30 +0300

    feat: ListFragment added

--- app/src/main/java/com/stevensekler/baker/bakingapp/FragmentsActivity.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/FragmentsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.stevensekler.baker.bakingapp.R;
+import com.stevensekler.baker.bakingapp.fragments.ListFragment;
 import com.stevensekler.baker.bakingapp.model.Cake;
 
 import static com.stevensekler.baker.bakingapp.MainActivity.CAKE_OBJECT;
@@ -17,14 +18,28 @@ public class FragmentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragments);
 
-        setTitle(getString(R.string.make_cake));
+        extractBundleData();
+        displayListFragment();
+    }
 
+    private void extractBundleData(){
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             cakeDetail = extras.getParcelable(CAKE_OBJECT);
+            setTitle(cakeDetail.getName());
         } else {
             finish();
             Toast.makeText(FragmentsActivity.this, R.string.no_data, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void displayListFragment(){
+        ListFragment listFragment = new ListFragment();
+
+//        firstFragment.setArguments(getIntent().getExtras());
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.list_fragment, listFragment).commit();
+
     }
 }
