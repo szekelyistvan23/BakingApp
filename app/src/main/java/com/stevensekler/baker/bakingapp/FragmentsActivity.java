4eee800e513dfff9dc9d4418e04a5f616074d715
Szekely Istvan <steven.sekler@gmail.com>
2018-03-28 14:18:03 +0300

    feat: Extract data from Bundle

--- app/src/main/java/com/stevensekler/baker/bakingapp/FragmentsActivity.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/FragmentsActivity.java
@@ -2,12 +2,27 @@ package com.stevensekler.baker.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.stevensekler.baker.bakingapp.R;
+import com.stevensekler.baker.bakingapp.model.Cake;
+
+import static com.stevensekler.baker.bakingapp.MainActivity.CAKE_OBJECT;
 
 public class FragmentsActivity extends AppCompatActivity {
+    private Cake cakeDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragments);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            cakeDetail = extras.getParcelable(CAKE_OBJECT);
+        } else {
+            finish();
+            Toast.makeText(FragmentsActivity.this, R.string.no_data, Toast.LENGTH_SHORT).show();
+        }
     }
 }
