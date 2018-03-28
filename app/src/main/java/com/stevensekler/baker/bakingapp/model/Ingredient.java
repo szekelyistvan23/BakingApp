538d6d7befef6394e2efe54d88156907b0f63530
Szekely Istvan <steven.sekler@gmail.com>
2018-03-28 14:10:28 +0300

    fix: Classes modified

--- app/src/main/java/com/stevensekler/baker/bakingapp/model/Ingredient.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/model/Ingredient.java
@@ -1,10 +1,13 @@
 package com.stevensekler.baker.bakingapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Szekely Istvan on 24.03.2018.
  */
 
-public class Ingredient {
+public class Ingredient implements Parcelable {
 
     private double quantity;
     private String measure;
@@ -33,4 +36,37 @@ public class Ingredient {
     public void setIngredient(String ingredient) {
         this.ingredient = ingredient;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(this.quantity);
+        dest.writeString(this.measure);
+        dest.writeString(this.ingredient);
+    }
+
+    public Ingredient() {
+    }
+
+    protected Ingredient(Parcel in) {
+        this.quantity = in.readDouble();
+        this.measure = in.readString();
+        this.ingredient = in.readString();
+    }
+
+    public static final Parcelable.Creator<Ingredient> CREATOR = new Parcelable.Creator<Ingredient>() {
+        @Override
+        public Ingredient createFromParcel(Parcel source) {
+            return new Ingredient(source);
+        }
+
+        @Override
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
 }
--- app/src/main/java/com/stevensekler/baker/bakingapp/model/Step.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/model/Step.java
@@ -1,10 +1,13 @@
 package com.stevensekler.baker.bakingapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Szekely Istvan on 24.03.2018.
  */
 
-public class Step {
+public class Step implements Parcelable {
 
     private int id;
     private String shortDescription;
@@ -51,4 +54,41 @@ public class Step {
     public void setThumbnailURL(String thumbnailURL) {
         this.thumbnailURL = thumbnailURL;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.shortDescription);
+        dest.writeString(this.description);
+        dest.writeString(this.videoURL);
+        dest.writeString(this.thumbnailURL);
+    }
+
+    public Step() {
+    }
+
+    protected Step(Parcel in) {
+        this.id = in.readInt();
+        this.shortDescription = in.readString();
+        this.description = in.readString();
+        this.videoURL = in.readString();
+        this.thumbnailURL = in.readString();
+    }
+
+    public static final Parcelable.Creator<Step> CREATOR = new Parcelable.Creator<Step>() {
+        @Override
+        public Step createFromParcel(Parcel source) {
+            return new Step(source);
+        }
+
+        @Override
+        public Step[] newArray(int size) {
+            return new Step[size];
+        }
+    };
 }
