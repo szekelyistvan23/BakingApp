e6a2daab910e4a15d3fe00a1b8018496db90b45d
Szekely Istvan <steven.sekler@gmail.com>
2018-04-10 18:41:21 +0300

    feat: ExoPlayer initial implementation

--- app/src/main/java/com/stevensekler/baker/bakingapp/fragments/DescriptionFragment.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/fragments/DescriptionFragment.java
@@ -2,6 +2,7 @@ package com.stevensekler.baker.bakingapp.fragments;
 
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.stevensekler.baker.bakingapp.R;
 import com.stevensekler.baker.bakingapp.model.Step;
 
@@ -36,11 +50,20 @@ public class DescriptionFragment extends Fragment {
     Button previousButton;
     @BindView(R.id.next_button)
     Button nextButton;
+    @BindView(R.id.place_of_video)
+    PlayerView exoPlayerView;
+    /** ExoPlayer implementation based on Google Codelab and ExoPlayer's Developer guide:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/#2
+     *  http://google.github.io/ExoPlayer/guide.html
+     * */
+    private SimpleExoPlayer player;
+    private boolean playWhenReady;
+    private int currentWindow;
+    private long playbackPosition;
     private Unbinder unbinder;
     private int arrayPosition;
     private Step[] steps;
     private boolean twoPane;
-
     PassDataToActivity callback;
 
     public DescriptionFragment() {
@@ -55,6 +78,7 @@ public class DescriptionFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_description, container, false);
         unbinder = ButterKnife.bind(this, view);
+        playWhenReady = true;
 
         if (getArguments().containsKey(STEP_ARRAY_POSITION)) {
             arrayPosition = getArguments().getInt(STEP_ARRAY_POSITION);
@@ -123,9 +147,58 @@ public class DescriptionFragment extends Fragment {
                 .commit();
     }
 
+    private void initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(getActivity()),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+        exoPlayerView.setPlayer(player);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(getActivity(),
+                Util.getUserAgent(getActivity(), "bakingapp"), bandwidthMeter);
+
+        String videoUrl = steps[arrayPosition].getVideoURL();
+
+        if (steps[arrayPosition] != null && videoUrl != null) {
+            Uri uri = Uri.parse(videoUrl);
+
+            MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+            player.prepare(mediaSource, true, false);
+        }
+    }
+
+    private void releasePlayer(){
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            callback = (PassDataToActivity) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement PassDataToActivity");
+        }
+    }
+
     @Override
     public void onStart() {
         super.onStart();
+
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
 
         if (!twoPane) {
             if (arrayPosition == 0) {
@@ -142,12 +215,26 @@ public class DescriptionFragment extends Fragment {
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-            callback = (PassDataToActivity) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString() + "must implement PassDataToActivity");
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
         }
     }
 
