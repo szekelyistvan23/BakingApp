fd285e81e9ae3debf462f1f25196b0dd4871a388
Szekely Istvan <steven.sekler@gmail.com>
2018-04-21 09:05:41 +0300

    feat: Widget configuration activity added

--- app/src/main/java/com/stevensekler/baker/bakingapp/IngredientWidget.java
+++ app/src/main/java/com/stevensekler/baker/bakingapp/IngredientWidget.java
@@ -7,13 +7,14 @@ import android.widget.RemoteViews;
 
 /**
  * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link IngredientWidgetConfigureActivity IngredientWidgetConfigureActivity}
  */
 public class IngredientWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = IngredientWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredient_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
@@ -27,6 +28,14 @@ public class IngredientWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (int appWidgetId : appWidgetIds) {
+            IngredientWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
         }
     }
 
